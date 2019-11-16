f5d1732b5c25dbc9bbe92a21b4e1cf938eb5f35f
Junior Suarez <juniorsuarez92@gmail.com>
2019-11-16 11:45:39 -0400

    Archivo de funciones

---
+++ inc/funciones.php
@@ -0,0 +1,60 @@
+<?php
+
+function url_base(){
+    //return 'http://localhost/ventas';
+    return 'http://10.0.10.25/ventas';
+}
+
+function print_pre($arr){
+    //if(isset($_GET) && $_GET['debug']){
+        echo '<pre>';
+            print_r($arr);   
+        echo '</pre>';  
+    //}
+}
+
+
+function selected($a,$b){
+    if($a == $b){
+        return 'selected';
+    }
+}
+
+function dateFormat($fecha,$tipo = 'fecha'){
+    $d = new DateTime($fecha);
+    if($tipo == 'hora'){
+        return $d->format('H:i:s');
+    }
+    return $d->format('d-m-Y');
+}
+
+function getItbis($pdo){
+
+    $query = "SELECT ITBIS,D_LEY FROM FABDPROC WHERE cod_sucu=1 AND cod_empr=1";
+    $result = Comando::recordSet($pdo,$query);
+
+    $_SESSION['itbis'] = $result[0]['ITBIS'];
+    $_SESSION['ley'] = $result[0]['D_LEY'];
+
+    echo "
+        <script>
+            sessionStorage.removeItem('itbis');
+            sessionStorage.removeItem('ley');
+            sessionStorage.setItem('itbis','".$result[0]['ITBIS']."');
+            sessionStorage.setItem('ley','".$result[0]['D_LEY']."');
+        </script>
+    ";
+
+}
+
+function getNumeroConCero($numero){
+
+    $cero = '0';
+    if($numero < 10){
+        return $numero = $cero.$numero;
+    }
+    return $numero;
+
+}
+
+?>
