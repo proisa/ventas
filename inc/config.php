eec47134acc7f1ee663c02e943013475f1a16892
Junior Suarez <juniorsuarez92@gmail.com>
2019-11-27 20:02:12 -0400

    Mejoras en cuadre

--- inc/config.php
+++ inc/config.php
@@ -2,7 +2,7 @@
 
 function url_base(){
     //return 'http://localhost/ventas';
-    return 'http://10.0.10.25/ventas';
+    return 'http://10.0.10.37/ventas';
 }
 
 function getConfig($pdo){
--- pages/cuadre_caja.php
+++ pages/cuadre_caja.php
@@ -69,7 +69,10 @@ endscan
 */
 
 $query = Comando::recordSet($pdo,"SELECT * FROM IVBDHETE  WHERE  he_tipfac!=''");
+$queryDeli = Comando::recordSet($pdo,"SELECT * FROM IVBDHEDELY  WHERE  he_tipfac!=''");
 
+//print_pre($queryDeli);
+//exit();
 //print_pre($query);
 
 $contado = 0;
@@ -109,6 +112,43 @@ foreach($query as $resp){
    $total_ventas += $total_ventas + $resp['HE_NETO'];
 }
 
+$contadoDL = 0;
+$creditoDL = 0;
+$chequeDL = 0;
+$tarjetaDL = 0;
+$baucherDL = 0;
+$nota_creditoDL = 0;
+$leyDL = 0;
+$total_ventasDL = 0;
+
+foreach($queryDeli as $resp){
+    if($resp['HE_TIPO'] == '2' || $resp['HE_TIPO'] == '5'){
+        if($resp['HE_DESC'] > 0 && ($resp['HE_ECHEQUE']+$resp['HE_ETARJE']+$resp['HE_BAUCHER']+ $resp['HE_VALNCRE']) == 0){
+             $contadoDL += $contadoDL + $resp['HE_NETO'];
+        }else{
+            $contadoDL += $contadoDL + ($resp['HE_NETO']-($resp['HE_ECHEQUE']+$resp['HE_ETARJE']+$resp['HE_BAUCHER']+ $resp['HE_VALNCRE']));
+        }
+ 
+        $chequeDL += $chequeDL + $resp['HE_ECHEQUE'];
+        $tarjetaDL += $tarjetaDL + $resp['HE_ETARJE'];
+        $nota_creditoDL += $nota_creditoDL + $resp['HE_VALNCRE'];
+        $baucherDL += $baucherDL + $resp['HE_BAUCHER'];
+ 
+        $leyDL += $leyDL + (($resp['HE_MONTO']-$resp['HE_DESC'])*($resp['HE_LEY']/100));
+ 
+    }elseif($resp['HE_TIPO'] == '1'){
+         $creditoDL += $creditoDL + $resp['HE_NETO'];
+    }elseif($resp['HE_TIPO'] == '3'){
+         $chequDL +=  $chequeDL + $resp['HE_NETO']; 
+         $leyDL += $leyDL + ($resp['HE_NETO'] * ($resp['HE_LEY']/100));
+    }elseif($resp['HE_TIPO'] == '4'){
+     $tarjetaDL += $tarjetaDL + $resp['HE_NETO'];
+     $leyDL += $leyDL + ($resp['HE_NETO'] * ($resp['HE_LEY']/100));
+    }
+ 
+    $total_ventasDL +=  $resp['HE_NETO'];
+ }
+
 $datalle = "      SELECT sum(a.de_cantid) as Cantidad,sum(a.de_cantid*de_precio) as Total,ISNULL(b.de_codigo,'') as de_codigo,
 isnull(C.AR_DESCRI,'') as NOMBREDEPARTAMENTO FROM IVBDDETE AS A 
 LEFT JOIN IVBDARTI as b ON A.AR_CODIGO=B.AR_CODIGO 
@@ -117,24 +157,44 @@ WHERE A.DE_CANTID>=0 AND A.DE_TIPFAC='C'
 GROUP BY B.DE_CODIGO,C.AR_DESCRI
 order by B.DE_codigo";
 
+/*
+  SELECT * FROM 
+    (
+      SELECT A.AR_CODIGO,A.DE_CANTID,A.DE_PRECIO,A.DE_DESCRI,b.de_codigo,C.AR_DESCRI,ISNULL(B.MA_CODIGO,'') as CATEGORIA  FROM IVBDDETE AS A LEFT JOIN IVBDARTI as b ON A.AR_CODIGO=B.AR_CODIGO 
+      LEFT JOIN IVBDDEPT AS C ON B.DE_CODIGO=C.DE_CODIGO 
+      WHERE A.DE_CANTID>=0 AND A.DE_TIPFAC='C' 
+
+      UNION ALL     
+
+      SELECT A.AR_CODIGO,A.DE_CANTID,A.DE_PRECIO,A.DE_DESCRI,b.de_codigo,C.AR_DESCRI,ISNULL(B.MA_CODIGO,'') as CATEGORIA  FROM IVBDDEDELY AS A LEFT JOIN IVBDARTI as b ON A.AR_CODIGO=B.AR_CODIGO 
+      LEFT JOIN IVBDDEPT AS C ON B.DE_CODIGO=C.DE_CODIGO 
+      WHERE A.DE_CANTID>=0 AND A.DE_TIPFAC='C' 
+      
+      ) AS BB order by ar_codigo
+
+*/
+
 $detalleResp = Comando::recordSet($pdo,$datalle);
 
 //print_pre($detalleResp);
 
 ?>
 <h1>Cuadre de caja</h1>
-<div class="box box-primary">
+
+<div class="row">
+    <div class="col-md-6">
+    <div class="box box-primary">
     <div class="box-header with-border">
-        <h3>Totales</h3>
+        <h4 class="t-cuadre">Ventas en Restaurante</h4>
     </div>
     <div class="box-body">
         <div class="row">
             <div class="col-md-12">
                 <table class="table table-striped">
                     <thead>
-                        <th>Ventas al contado</th>
-                        <th>Ventas a credito</th>
-                        <th>Total de ventas</th>
+                        <th>Ventas al Contado</th>
+                        <th>Ventas a Credito</th>
+                        <th>Total en Restaurante</th>
                     </thead>
                     <tbody>
                         <tr>
@@ -144,6 +204,46 @@ $detalleResp = Comando::recordSet($pdo,$datalle);
                         </tr>
                     </tbody>
                 </table>
+            </div>
+        </div>
+    </div>
+</div>
+    </div>
+    <div class="col-md-6">
+        
+<div class="box box-primary">
+    <div class="box-header with-border">
+        <h4 class="t-cuadre">Ventas en Delivery</h4>
+    </div>
+    <div class="box-body">
+        <div class="row">
+            <div class="col-md-12">
+                <table class="table table-striped">
+                    <thead>
+                        <th>Ventas al Contado</th>
+                        <th>Ventas a Credito</th>
+                        <th>Total en Delivery</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>RD$ <?=number_format($contadoDL,2)?></td>
+                            <td>RD$ <?=number_format($creditoDL,2)?></td>
+                            <td>RD$ <?=number_format($total_ventasDL,2)?></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+    </div>
+</div>
+
+<div class="box box-primary">
+    <div class="box-body">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                    <h3 class=" t-cuadre">Total de Ventas: RD$ <?=number_format($total_ventas + $total_ventasDL,2)?> </h3>
             </div>
         </div>
     </div>
@@ -160,19 +260,19 @@ $detalleResp = Comando::recordSet($pdo,$datalle);
                     <tbody>
                         <tr>
                             <td>Efectivo</td>
-                            <td>RD$ <?=number_format($contado,2)?></td>
+                            <td>RD$ <?=number_format($contado + $contadoDL,2)?></td>
                         </tr>
                         <tr>
                             <td>Cheque</td>
-                            <td>RD$ <?=number_format($cheque,2)?></td>
+                            <td>RD$ <?=number_format($cheque +  $chequeDL,2)?></td>
                         </tr>
                         <tr>
                             <td>Tarjeta</td>
-                            <td>RD$ <?=number_format($tarjeta,2)?></td>
+                            <td>RD$ <?=number_format($tarjeta + $tarjetaDL,2)?></td>
                         </tr>
                         <tr>
                             <td>Notas de credito</td>
-                            <td>RD$ <?=number_format($nota_credito,2)?></td>
+                            <td>RD$ <?=number_format($nota_credito + $nota_creditoDL,2)?></td>
                         </tr>
                     </tbody>
                 </table>
